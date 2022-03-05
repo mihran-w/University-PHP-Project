aef241304c0769a6b6fc2cb405bc383f79e70943
mihran-w <mihranw@gmail.com>
2022-03-05 15:29:58 +0330

    Create DataBase by mySql And Connection String

--- inc/footer.php
+++ inc/footer.php
@@ -18,7 +18,6 @@
         </div>
     </footer>
     <script src="./assets/js/bootstrap.bundle.min.js"></script>
-    <script src="./assets/js/Jquery.min.js"></script>
     <script src="./assets/js/main.js"></script>
 </body>
 
