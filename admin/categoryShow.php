c8194dcc8da8374c24c5bd2f32967ad9fb15b673
mihran-w <mihranw@gmail.com>
2022-04-06 22:19:47 +0430

    Categories Show Page

---
+++ admin/categoryShow.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+if (!$_SESSION['username']) {
+    header('location:../login.php');
+}
+?>
+
+<!-- Header -->
+<?php
+include_once('./inc/header.php')
+?>
+
+<?php
+require_once("../inc/config.php");
+$sql = "SELECT * FROM categories";
+$result = mysqli_query($conn, $sql);
+?>
+
+
+<div class="container mt-5">
+    <div class="mt-5">
+        <h1>لیست دسته بندی ها</h1>
+        <hr>
+        <table class="table table-bordered table-hover table-striped table-primary mt-5">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">نام دسته بندی</th>
+                    <th scope="col">توضیحات</th>
+                    <th scope="col">عملیات</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $counter = 0;
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $counter += 1;
+                ?>
+                    <tr>
+                        <th scope="row"><?= $counter ?></th>
+                        <td><?= $row['name'] ?></td>
+                        <td><?= $row['description'] ?></td>
+                        <td><a href="categoryDelete.php?id=<?= $row['id'] ?>" class="btn btn-danger">حذف</a></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<div style="height: 7rem;"></div>
+
+
+<!-- Footer -->
+<?php
+include_once('./inc/footer.php')
+?>
