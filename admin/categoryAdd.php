693f5dca508e24abb3a4f0afe4b7738b4b23636f
mihran-w <mihranw@gmail.com>
2022-04-06 22:18:23 +0430

    Category add Page

---
+++ admin/categoryAdd.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+if (!$_SESSION['username']) {
+    header('location:../login.php');
+}
+?>
+
+<!-- Header -->
+<?php
+include_once('./inc/header.php')
+?>
+
+<div class="container mt-5 mb-5">
+    <h1>افزودن دسته بندی جدید</h1>
+    <hr>
+    <div class="container">
+        <form action="add.php" method="POST" class="mt-5">
+            <div class="mb-2">
+                <label for="name" class="form-label">نام دسته بندی :</label>
+                <input type="text" class="form-control" id="name" placeholder="نام دسته بندی را وارد کنید" name="name">
+            </div>
+            <div class="mb-2">
+                <label for="description" class="form-label">توضیحات :</label>
+                <textarea class="form-control" id="description" rows="8" name="description" placeholder="توضیحات دسته بندی را وارد کنید"></textarea>
+            </div>
+            <div class="mb-2">
+                <button type="submit" class="btn btn-success">ثبت دسته بندی</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+<div style="height: 7rem;"></div>
+
+
+
+<!-- Footer -->
+<?php
+include_once('./inc/footer.php')
+?>
